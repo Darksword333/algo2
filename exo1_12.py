c048d4e3dc00b517eb488125026b2d47c30a39f9
Darksword333 <mazetgabriel@gmail.com>
2025-04-11 11:53:28 +0200

    Exercice 12

--- exo1_12.py
+++ exo1_12.py
@@ -2,12 +2,15 @@
 
 def recherche_lin(A : list[int], X: int) -> int:
     #@ requires len(A) > 0
-    #@ ensures result == result
-    pos = -1
+    #@ ensures (result == -1 or A[result] == X)
+    #@ ensures (result != -1 -> not isIn(X, A))
     i = len(A)-1
+    pos = -1
     for i in range(-1,len(A), -1):
         #@ variant i
         #@ invariant pos == -1 or A[pos] == X
+        #@ invariant forall i. pos < i < len(A) -> A[i] != X
         if A[i] == X:
+            pos = i
             return pos
     return pos
